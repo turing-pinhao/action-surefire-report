87758da07c8fc72ffd3609620b8f96af3f1b78ec
turing-pinhao <77177676+turing-pinhao@users.noreply.github.com>
2021-02-16 22:35:05 -0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -49,7 +49,7 @@ const action = async () => {
     }
     else
     {
-        core.setOutput('annotation', annotations.join('|'));
+        core.setOutput('annotation', JSON.stringify(annotations, null, 2));
     }    
 
     // optionally fail the action if tests fail
